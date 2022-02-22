ba06587b4d248c35dd55be69920d6346b449c2a8
bb2100 <albin.ekelunden@gmail.com>
2022-02-22 15:02:22 +0100

    Made the code work

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import resources
-from resources import Character, Goblin
+from resources import Character, Goblin, save_character, load_characters
 from random import randint, shuffle, choice
 
 def fight(players : list, enemies : list):
@@ -32,11 +32,12 @@ def fight(players : list, enemies : list):
 
 if __name__ == "__main__":
     enemies = []
-    players = []
-    emy = Character("Emy", 20, 5, 2)
-    nick = Character("nick", 15, 2, 1)
-    players.append(emy)
-    players.append(nick)
+    players = load_characters()
+
+    #emy = Character("Emy", 20, 5, 2)
+    #nick = Character("nick", 15, 2, 1)
+    #players.append(emy)
+    #players.append(nick)
 
     enemies.append(Goblin(10, 3, 2, 1))
     enemies.append(Goblin(15, 2, 1, 2))
@@ -51,5 +52,13 @@ if __name__ == "__main__":
 
     if len(enemies) ==0:
         print("The players won!")
+        print("would you like to save the ramaining characters?")
+        save_progress = input("(y/n):")
+        if save_progress == "y":
+            for player in players:
+                save_character(player)
+        else:
+            print("No progress was saved")
+        
     elif len(players) == 0:
         print("The Goblins won!")
